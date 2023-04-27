4302b3a1a49f877fa399b496f8d76ea3beaf9667
ThaoDTD <49466171+thaodtd-0665@users.noreply.github.com>
2023-04-27 13:10:38 +0700

    create detail product page

--- ecommerceSite/app/urls.py
+++ ecommerceSite/app/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('register/', views.register, name="register"),
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutPage, name="logout"),
+    path('products/', views.productList, name='products'),
+    path('product/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
+
 ]
